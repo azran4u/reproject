544afffbf372c4677c6c3c2f729fb3daf9341c39
azran4u <azran4u@gmail.com>
2019-03-19 22:07:34 +0200

    fix convertion

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-// import {dirty_reprojectors} from "dirty-reprojectors";
 import {GeoProjection, geoEquirectangular, geoMercator} from "d3-geo";
-import { exists } from "fs";
-// import * as dirty from '../node_modules/dirty-reprojectors';
 
 const proj4326: GeoProjection = geoEquirectangular();
 const proj3857: GeoProjection = geoMercator();
 
 // const coordinate: [number, number] = [34.956806299325386, 32.82589141525937]; // Haifa
 const coordinate: [number, number] = [35.19071102142334, 33.27171288126284]; // Zor
+console.log( "Original LatLng: [" + coordinate + "]");
 
 const epsg4326XY: [number, number] = proj4326(coordinate);
-console.log( "LatLng to EPSG4326 [" + coordinate + "] is:[" +  epsg4326XY + "]");
+console.log( "EPSG4326 XY = [" + epsg4326XY + "]");
 
-const latlon4326: [number, number] = proj4326.invert(epsg4326XY);
-console.log( "EPSG4326 to LatLng [" + epsg4326XY + "] is:[" + proj4326.invert(epsg4326XY) + "]" );
+const latLng3857: [number, number] = proj3857.invert(epsg4326XY);
+console.log( "EPSG4326 reverse to LatLng (for Mapbox) = [" + latLng3857 + "]" );
 
-const epsg3857LatLng: [number, number] = proj3857.invert(epsg4326XY);
-console.log( "LatLng for mercator [" + epsg3857LatLng + "]" );
-
-const epsg3857XY: [number, number] = proj3857(epsg3857LatLng);
-console.log( "EPSG3857 XY = [" + epsg3857XY + "]" );
+const epsg3857XY: [number, number] = proj3857(latLng3857);
+console.log( "EPSG3857 XY of the reverset LatLng = [" + epsg3857XY + "]" );
